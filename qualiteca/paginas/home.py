563a3626ac22d0c597f48425cba8cbf015b351ac
Ramilton Lima <ramilton.silva.lima@live.com>
2023-11-15 18:43:23 -0300

    Ajustes Home

--- qualiteca/paginas/home.py
+++ qualiteca/paginas/home.py
@@ -8,7 +8,5 @@ class Home:
         super().__init__()
         st.header('Qualiteca', anchor=False, divider='orange')
         st.markdown(
-            'Biblioteca formada com doações de livros do call center para incentivar a leitura de todos. ' +
-            'Livros teremos o empréstimo de acordo como tamanho do livro, gibis e revistas ' +
-            'ficarão dentro da descompressão para leitura rápida quando estiverem lá.'
+            'Biblioteca formada com doações de livros'
         )
